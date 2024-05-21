25df40d5d7e36280279f1b6ff5dfa7770fbf021f
b1ack-panther <saurabh8604vns@gmail.com>
2024-05-21 15:24:14 +0530

    url changes

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-	"http://localhost:3000",
-	"http://localhost:5173",
-	"https://www.dandrepairshop.com",
-	"https://dandrepairshop.com",
+	// "http://localhost:3000",
+	// "http://localhost:5173",
+	// "https://www.dandrepairshop.com",
+	// "https://dandrepairshop.com",
+	"https://tech-notes-ten.vercel.app/",
 ];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
